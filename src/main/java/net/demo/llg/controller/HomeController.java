2d6ae951882e10ea30edc1b3b15a93069a8c5d2c
lvliguo <lvliguo@tp-link.net>
2016-10-19 10:44:48 +0800

    dev test

--- src/main/java/net/demo/llg/controller/HomeController.java
+++ src/main/java/net/demo/llg/controller/HomeController.java
@@ -1,5 +1,6 @@
 package net.demo.llg.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,15 +18,20 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class HomeController {
+
+	@Value("${mail.from}")
+	private String mailAddress;
 	
 	/**
 	 * 主页
-	 * @return
+	 *
+	 * @return mv
 	 */
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public ModelAndView getHome() {
 		ModelAndView mv = new ModelAndView("home");
 		mv.addObject("name", "Lv li guo");
+		mv.addObject("mailAddress", mailAddress);
 
 		return mv;
 	}
